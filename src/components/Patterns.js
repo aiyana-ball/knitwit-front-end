6b9294d326710db60e9ca7b78d3802ddbab3d834
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-02 16:59:14 -0700

    Created a new Patterns component

---
+++ src/components/Patterns.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Patterns() {
+  return (
+    <div>
+      <h1>Patterns Page</h1>
+      <p>Welcome to the patterns page!</p>
+    </div>
+  );
+}
+
+export default Patterns;
