75115e2b1f83482fed8e02dcad8bf277115aa3ec
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-02 16:55:39 -0700

    Imported and linked/routed new components, moved Google sign-in to Profile page

--- src/App.js
+++ src/App.js
@@ -2,42 +2,61 @@ import './App.css';
 import './components/SearchBar.css';
 import './components/MenuButtons.css';
 import UserContext from './UserContext';
-import db from './firebase.js';
-import { getAccountData } from './firebase.js';
-import { getAuth, GoogleAuthProvider, signInWithGoogle} from './firebase.js';
+import { db, getAccountData, getAuth, GoogleAuthProvider, signInWithGoogle} from './firebase.js';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Patterns from './components/Patterns';
+import YarnPage from './components/Yarn';
 
 function App() {
-
-  getAccountData();
+  const [user, setUser] = useState(null); //If the user data updates, re-render
+  //this line adds the user photo {user && <img src={user.photoURL} alt="User" />}  (to be used later)
+  const handleSignIn = () => {
+    signInWithGoogle().then(user => setUser(user));
+  };
   return (
-    <div className="container">
-      <div className="menu">
-        <div className='logo-placeholder'></div>
-        <button className="sidebar-button">Home</button>
-        <button className="sidebar-button">Profile</button>
-        <button className="sidebar-button">Patterns</button>
-        <button className="sidebar-button">Yarn</button>
-      </div>
-      <button onClick={() => signInWithGoogle()}>Sign in with Google</button>
-      <div className="header">
-        <div id="cover">
-          <form method="get" action="">
-            <div className="tb">
-              <div className="td"><input type="text" placeholder="Search" required/></div>
-              <div className="td" id="s-cover">
-                <button type="submit">
-                  <div id="s-circle"></div>
-                  <span></span>
-                </button>
-              </div>
+    <UserContext.Provider value={user}> {/*Give every component access to the user data context provider*/}
+      <Router>
+        <div className="container">
+          <div className="menu">
+            <div className="logo-placeholder">
+              <img src="/knitwit-logo.png" alt="Logo" />
             </div>
-          </form>
+            <Link to="/home"><button className="sidebar-button">Home</button></Link>
+            <Link to="/profile"><button className="sidebar-button">Profile</button></Link>
+            <Link to="/patterns"><button className="sidebar-button">Patterns</button></Link>
+            <Link to="/yarn"><button className="sidebar-button">Yarn</button></Link>
+          </div>
+          <div className="header">
+            <div id="cover">
+              <form method="get" action="">
+                <div className="tb">
+                  <div className="td"><input type="text" placeholder="Search" required/></div>
+                  <div className="td" id="s-cover">
+                    <button type="submit">
+                      <div id="s-circle"></div>
+                      <span></span>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="main">
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile" element={<Profile onSignIn={handleSignIn} />} />
+              <Route path="/patterns" element={<Patterns />} />
+              <Route path="/yarn" element={<YarnPage />} />
+            </Routes>
+          </div>
+          {/* <div className="main">Main Content</div> */}
+          <div className="footer">Footer</div>
         </div>
-      </div>
-      <div className="main">Main Content</div>
-      <div className="footer">Footer</div>
-    </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
