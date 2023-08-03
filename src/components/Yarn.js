4de94732e1053cd030df338fef8bff4c1482923f
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-02 17:00:00 -0700

    Created a new Yarn component

---
+++ src/components/Yarn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function YarnPage() {
+  return (
+    <div>
+      <h1>Yarn Page</h1>
+      <p>Welcome to the yarn page!</p>
+    </div>
+  );
+}
+
+export default YarnPage;
