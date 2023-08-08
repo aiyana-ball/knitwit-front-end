928712c048d526b75085568c4e72a168be37fcdf
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-08 11:21:45 -0700

    Added getAllPatterns in useEffect, render results to pattern page

--- src/components/Patterns.js
+++ src/components/Patterns.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllPatterns } from './ravelry';
 
 function Patterns() {
+  const [patternData, setPatternData] = useState(null);
+
+  useEffect(() => {
+    getAllPatterns()
+      .then(data => setPatternData(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  if (!patternData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>Patterns Page</h1>
       <p>Welcome to the patterns page!</p>
+      <div className="grid">
+        {Object.entries(patternData.patterns).map(([key, pattern]) => (
+          <div key={pattern.id} className="grid-item">
+            <h2>{pattern.name}</h2>
+            <img src={pattern.image_url} alt={pattern.name} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
