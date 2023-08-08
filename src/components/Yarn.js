ff7872355211c7b8cc1e83185ff9f46b7d4ad048
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-08 11:22:56 -0700

    Added getAllYarns in useEffect, render results to yarn page

--- src/components/Yarn.js
+++ src/components/Yarn.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllYarns } from './ravelry';
 
 function YarnPage() {
+  const [yarns, setYarns] = useState([]);
+
+  useEffect(() => {
+    getAllYarns('yarns/search.json')
+      .then(data => setYarns(data.yarns))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-    <div>
-      <h1>Yarn Page</h1>
-      <p>Welcome to the yarn page!</p>
+    <div className="yarns">
+      {yarns.map((yarn, index) => (
+        <div key={index} className="yarn">
+          <img src={yarn.image_url} alt={yarn.name} />
+          <h2>{yarn.name}</h2>
+        </div>
+      ))}
     </div>
   );
 }
