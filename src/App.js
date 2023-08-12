78136b20bfccbf68996af98d96cbe118133981c8
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-11 23:58:29 -0700

    Moved headers from main to header, saved search results

--- src/App.js
+++ src/App.js
@@ -11,15 +11,23 @@ import Search from './components/Search';
 import Profile from './components/Profile';
 import Patterns from './components/Patterns';
 import YarnPage from './components/Yarn';
+import SearchBar from './components/SearchBar';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [searchResults, setSearchResults] = useState(JSON.parse(localStorage.getItem('searchResults')) || null);
+
   const handleSignIn = () => {
     signInWithGoogle().then(user => setUser(user));
   };
 
   const handleSignOut = () => {
     firebaseSignOut().then(() => setUser(null));
+  };
+
+  const handleSearch = (results) => {
+    setSearchResults(results);
+    localStorage.setItem('searchResults', JSON.stringify(results));
   };
 
   useEffect(() => {
@@ -30,9 +38,15 @@ function App() {
   }, []);
 
   return (
-    <UserContext.Provider value={{ user, signIn: handleSignIn, signOut: handleSignOut }}> {/*Give every component access to the user data context provider*/}
+    <UserContext.Provider value={{ user, signIn: handleSignIn, signOut: handleSignOut }}> 
       <Router>
         <div className="container">
+          <div className="header">
+          <h1 className='welcome-knitwit'>Welcome to KnitWit!</h1>
+            <div id="cover">
+              <SearchBar onSearch={handleSearch} />
+            </div>
+          </div>
           <div className="menu">
             <div className="logo-placeholder">
               <img src="/knitwit-logo.png" alt="Logo" />
@@ -42,21 +56,15 @@ function App() {
             <Link to="/patterns"><button className="sidebar-button">Patterns</button></Link>
             <Link to="/yarn"><button className="sidebar-button">Yarn</button></Link>
           </div>
-          {/* <div className="header">
-            <div id="cover">
-              <SearchBar />
-            </div>
-          </div> */}
           <div className="main">
             <Routes>
               <Route path="/" element={<Navigate to="/search" />} />
-              <Route path="/search" element={<Search />} />
+              <Route path="/search" element={<Search searchResults={searchResults}/>} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/patterns" element={<Patterns />} />
               <Route path="/yarn" element={<YarnPage />} />
             </Routes>
           </div>
-          {/* <div className="main">Main Content</div> */}
           <div className="footer">Footer</div>
         </div>
       </Router>
