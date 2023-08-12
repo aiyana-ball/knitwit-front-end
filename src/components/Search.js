5d87ca73806d428fd4d35f5dd4aaeff700529bdf
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-12 00:02:02 -0700

    Added zipper merge, refactored return and added favorite button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,50 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './SearchBar.css';
-import SearchBar from './SearchBar';
 import './Search.css';
+import Favorites from './Favorites';
 
-function Search() {
-  const [searchResults, setSearchResults] = useState(JSON.parse(localStorage.getItem('searchResults')) || null);
+function Search({ searchResults }) {
+  const [combinedResults, setCombinedResults] = useState([]);
 
-  const handleSearch = (results) => {
-    setSearchResults(results);
-    localStorage.setItem('searchResults', JSON.stringify(results))
-  };
-
-  let combinedResults = [];
-  if (searchResults && searchResults.patterns && searchResults.patterns.patterns && searchResults.yarns && searchResults.yarns.yarns) {
-    const patterns = searchResults.patterns.patterns;
-    const yarns = searchResults.yarns.yarns;
-    for (let i = 0; i < patterns.length; i++) {
-      if (yarns[i]) {
-        combinedResults.push({ type: 'yarn', data: yarns[i] });
-      }
+  useEffect(() => {
+    // console.log(searchResults);
+    let combinedResults = [];
+    if (searchResults && searchResults.patterns && searchResults.patterns.patterns && searchResults.yarns && searchResults.yarns.yarns) {
+      const patterns = searchResults.patterns.patterns;
+      const yarns = searchResults.yarns.yarns;
+      //console.log(patterns);
+      for (let i = 0; i < Math.max(patterns.length, yarns.length); i++) {
       if (patterns[i]) {
         combinedResults.push({ type: 'pattern', data: patterns[i] });
       }
+      if (yarns[i]) {
+        combinedResults.push({ type: 'yarn', data: yarns[i] });
+      }
     }
-  }
+  } 
+    setCombinedResults(combinedResults);
+  }, [searchResults]);
 
   return (
     <div>
-      <div className="header">
-        <div id="cover">
-          <SearchBar onSearch={handleSearch} />
-        </div>
-      </div>
-      <h1>Search Page</h1>
-      <p>Welcome to the Search page! Search for anything here.</p>
+      {/* <h1>Search Page</h1>
+      <p>Welcome to the Search page! Search for anything here.</p> */}
       {combinedResults && (
         <div className="search-results">
-          {combinedResults.map((result, index) => (
-            <div key={index} className="search-result">
-              {result.data.first_photo && <img src={result.data.first_photo.small_url} alt={result.data.name}/>}
-              <h2>{result.data.name}</h2>
-            </div>
-          ))}
+          {combinedResults.map((result, index) => {
+            //console.log('Result data:')
+            //console.log(result.data);
+            return <Favorites key={index} item={result.data} type={result.type}/>
+            // <div key={index} className="search-result">
+            //   {result.data.first_photo && <img src={result.data.first_photo.small_url} alt={result.data.name}/>}
+            //   <h2>{result.data.name}</h2>
+            // </div>
+          })}
         </div>
       )}
-    </div>
+      </div>
   );
 }
 
