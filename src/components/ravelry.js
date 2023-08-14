0216ac4c86c152dd98203508be7eceb071f73e63
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-13 18:09:00 -0700

    Cleaned up code, removed console logs

--- src/components/ravelry.js
+++ src/components/ravelry.js
@@ -11,33 +11,27 @@ export function makeRavelryRequest(endpoint) {
     return response.json();
     })
     .then(data => {
-      // console.log(data);
       return data;
     });
   }
 
-    
   export function getAllPatterns() {
-    console.log('Getting all patterns')
     return makeRavelryRequest(`patterns/search.json?&page_size=50`)
       .then(data => {
         data.patterns.forEach(pattern => {
           pattern.type = 'pattern';
         });
-        // console.log(data);
         return data;
       })
       .catch(error => console.error('Error:', error));
   }
   
   export function getAllYarns() {
-    console.log('Getting all yarns')
     return makeRavelryRequest(`yarns/search.json?&page_size=50`)
       .then(data => {
         data.yarns.forEach(yarn => {
           yarn.type = 'yarn';
         });
-        // console.log(data);
         return data;
       })
       .catch(error => console.error('Error:', error));
