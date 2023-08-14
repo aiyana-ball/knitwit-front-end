943951f13371e8e501cafca96155d1a86fe5a197
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-13 18:08:06 -0700

    Added link to item if it exists, changed heart button to white if not clicked

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,50 +1,73 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import UserContext from '../UserContext';
-import { doc, getDoc, updateDoc, arrayUnion, arrayRemove, setDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, arrayUnion, arrayRemove, setDoc, onSnapshot } from 'firebase/firestore';
 import { db } from './firebase';
 import './SearchResults.css';
 
 function Favorites({ item }) {
   const { user } = useContext(UserContext);
+  const [isFavorited, setIsFavorited] = useState(false);
   
-  // console.log(user)
-  const toggleFavorite = async () => {
-    // console.log("toggleFavorite")
+  useEffect(() => {
+    let unsubscribe;
     if (user) {
       const userRef = doc(db, 'account', user.uid);
-      // console.log("user exists")
+      unsubscribe = onSnapshot(userRef, (docSnap) => {
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          if (userData.favorites && userData.favorites.some(favorite => favorite.id === item.id)) {
+            setIsFavorited(true);
+          } else {
+            setIsFavorited(false);
+          }
+        }
+      });
+    }
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, [user, item]);
+  
+  const toggleFavorite = async () => {
+    if (user) {
+      const userRef = doc(db, 'account', user.uid);
       const docSnap = await getDoc(userRef);
       const favoriteItem = { id: item.id, type: item.type };
 
       if (docSnap.exists()) {
-        // console.log("docSnap exists")
         const userData = docSnap.data();
         if (userData.favorites && userData.favorites.some(favorite => favorite.id === item.id)) {
-          // console.log("favorites includes item")
           await updateDoc(userRef, {
             favorites: arrayRemove(favoriteItem)
           });
         } else {
-          // console.log("favorites does not include item")
           await updateDoc(userRef, {
             favorites: arrayUnion(favoriteItem)
           }).catch(error => console.error('Error updating document:', error));
         }
       } else {
-        // console.log("favorites field doesn't exist")
         await setDoc(userRef, {
           favorites: [favoriteItem]
         }, { merge: true });
+        setIsFavorited(true);
       }
     }
   };
 
   return (
-      <div className="search-result">
-        {item.first_photo && <img src={item.first_photo.small_url} alt={item.name}/>}
-        <h2>{item.name}</h2>
-        {user && <button className="heart-button" onClick={toggleFavorite}>❤️</button>}
-      </div>
+    <div className="search-result">
+      <img src={item.first_photo ? item.first_photo.small_url : '/knitwit-logo.png'} alt={item.name}/>
+      <h2>
+        {item && item.download_location && item.download_location.url 
+          ? <a href={item.download_location.url}>{item.name}</a> 
+          : item.yarn_company && item.yarn_company.url 
+            ? <a href={item.yarn_company.url}>{item.name}</a> 
+            : item.name}
+      </h2>
+      {user && <button className="heart-button" onClick={toggleFavorite}>{isFavorited ? '❤️' : '🤍'}</button>}
+    </div>
   );
 }
 
