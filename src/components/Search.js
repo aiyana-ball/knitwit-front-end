ec07efcc63e382c2729d6ed1a97ea763a51f6e8d
aiyana-ball <121783190+aiyana-ball@users.noreply.github.com>
2023-08-13 18:08:30 -0700

    Added types to combined results

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,17 +7,17 @@ function Search({ searchResults }) {
   const [combinedResults, setCombinedResults] = useState([]);
 
   useEffect(() => {
-    // console.log(searchResults);
     let combinedResults = [];
     if (searchResults && searchResults.patterns && searchResults.patterns.patterns && searchResults.yarns && searchResults.yarns.yarns) {
       const patterns = searchResults.patterns.patterns;
       const yarns = searchResults.yarns.yarns;
-      //console.log(patterns);
       for (let i = 0; i < Math.max(patterns.length, yarns.length); i++) {
       if (patterns[i]) {
+        patterns[i].type = 'pattern';
         combinedResults.push({ type: 'pattern', data: patterns[i] });
       }
       if (yarns[i]) {
+        yarns[i].type = 'yarn';
         combinedResults.push({ type: 'yarn', data: yarns[i] });
       }
     }
@@ -27,18 +27,10 @@ function Search({ searchResults }) {
 
   return (
     <div>
-      {/* <h1>Search Page</h1>
-      <p>Welcome to the Search page! Search for anything here.</p> */}
       {combinedResults && (
         <div className="search-results">
           {combinedResults.map((result, index) => {
-            //console.log('Result data:')
-            //console.log(result.data);
             return <Favorites key={index} item={result.data} type={result.type}/>
-            // <div key={index} className="search-result">
-            //   {result.data.first_photo && <img src={result.data.first_photo.small_url} alt={result.data.name}/>}
-            //   <h2>{result.data.name}</h2>
-            // </div>
           })}
         </div>
       )}
